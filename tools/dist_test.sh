49884bbd887725ccd6f298b4c173173d2f925813
Leaf <leaf.ying.work@gmail.com>
2023-08-02 23:47:04 +0800

    Changed - to _ in dist_train.sh and dist_test.sh script

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -12,12 +12,12 @@ MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 PYTHONPATH="$(dirname "$0")/..:$PYTHONPATH" \
 torchrun \
     --nnodes "$NNODES" \
-    --node-rank "$NODE_RANK" \
-    --nproc-per-node "$GPUS" \
-    --rdzv-id "$JOB_ID" \
-    --rdzv-backend "c10d" \
-    --master-addr="$MASTER_ADDR" \
-    --master-port="$PORT" \
+    --node_rank "$NODE_RANK" \
+    --nproc-per_node "$GPUS" \
+    --rdzv_id "$JOB_ID" \
+    --rdzv_backend "c10d" \
+    --master_addr="$MASTER_ADDR" \
+    --master_port="$PORT" \
     "$(dirname "$0")/train.py" \
     "$CONFIG" \
     "$CHECKPOINT" \
