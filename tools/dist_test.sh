b2bd98b5484178b696a7736a18018bd2abca435a
Leaf <leaf.ying.work@gmail.com>
2023-08-15 18:42:04 +0800

    Fixed incorrect dist_test.sh script

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -13,12 +13,12 @@ PYTHONPATH="$(dirname "$0")/..:$PYTHONPATH" \
 torchrun \
     --nnodes "$NNODES" \
     --node_rank "$NODE_RANK" \
-    --nproc-per_node "$GPUS" \
+    --nproc_per_node "$GPUS" \
     --rdzv_id "$JOB_ID" \
     --rdzv_backend "c10d" \
     --master_addr="$MASTER_ADDR" \
     --master_port="$PORT" \
-    "$(dirname "$0")/train.py" \
+    "$(dirname "$0")/test.py" \
     "$CONFIG" \
     "$CHECKPOINT" \
     --launcher pytorch \
